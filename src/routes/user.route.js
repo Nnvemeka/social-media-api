3c64206f2898f9e4ada1b5775b7272e25c687e2c
nnvedward <nnvedward@gmail.com>
2022-05-21 14:01:04 +0100

    Fix: Added authentication middleware to follow and unfollow user route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const UserCtrl = require('../controllers/user.controller')
-const auth = require('../middlewares/auth.middleware')
+const { verifyToken, auth } = require('../middlewares/auth.middleware')
 
 router.patch('/:userId', auth, UserCtrl.updateUser)
 router.delete('/:userId', auth, UserCtrl.deleteUser)
 router.get('/:userId', auth, UserCtrl.getUser)
-router.patch('/:userId/follow', UserCtrl.followUser)
-router.patch('/:userId/unfollow', UserCtrl.unFollowUser)
+router.patch('/:userId/follow', verifyToken, UserCtrl.followUser)
+router.patch('/:userId/unfollow', verifyToken, UserCtrl.unFollowUser)
 
 module.exports = router
